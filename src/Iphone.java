f46d34c6669ebba79ba9f5b3ff332ce7b0dbb721
agsilvamhm <agsilva.mhm@gmail.com>
2025-02-18 07:14:20 -0300

    Primeiro commit - adicionando projeto

---
+++ src/Iphone.java
@@ -0,0 +1,66 @@
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+    @Override
+    public void tocar() {
+        System.out.println("Reproduzindo música...");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Música pausada.");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Música selecionada: " + musica);
+    }
+
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para " + numero + "...");
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Chamada atendida.");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz...");
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada.");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Página atualizada.");
+    }
+
+    // Método principal para testar as funcionalidades
+    public static void main(String[] args) {
+        Iphone meuIphone = new Iphone();
+
+        // Testando o Reprodutor Musical
+        meuIphone.selecionarMusica("Imagine - John Lennon");
+        meuIphone.tocar();
+        meuIphone.pausar();
+
+        // Testando o Aparelho Telefônico
+        meuIphone.ligar("11999999999");
+        meuIphone.atender();
+        meuIphone.iniciarCorreioVoz();
+
+        // Testando o Navegador na Internet
+        meuIphone.exibirPagina("https://www.apple.com");
+        meuIphone.adicionarNovaAba();
+        meuIphone.atualizarPagina();
+    }
+}
